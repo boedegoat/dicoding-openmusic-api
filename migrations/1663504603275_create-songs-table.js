18884731d8ad298bfe135b308c25a89d8850b54a
boedegoat <bhremada.fka@gmail.com>
2022-09-18 19:37:58 +0700

    separate migrations

---
+++ migrations/1663504603275_create-songs-table.js
@@ -0,0 +1,48 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+    pgm.createTable("songs", {
+        id: {
+            type: "VARCHAR(50)",
+            primaryKey: true,
+        },
+        album_id: {
+            type: "VARCHAR(30)",
+            references: '"albums"',
+            onDelete: "cascade",
+        },
+        title: {
+            type: "TEXT",
+            notNull: true,
+        },
+        year: {
+            type: "INTEGER",
+            notNull: true,
+        },
+        genre: {
+            type: "TEXT",
+            notNull: true,
+        },
+        performer: {
+            type: "TEXT",
+            notNull: true,
+        },
+        duration: {
+            type: "INTEGER",
+        },
+        created_at: {
+            type: "TIMESTAMP",
+            notNull: true,
+            default: pgm.func("current_timestamp"),
+        },
+        updated_at: {
+            type: "TIMESTAMP",
+            notNull: true,
+            default: pgm.func("current_timestamp"),
+        },
+    });
+};
+
+exports.down = (pgm) => {
+    pgm.dropTable("songs");
+};
